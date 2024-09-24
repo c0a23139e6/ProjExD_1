08694b4019b3671137ea30769241552df7c86dcd
proje <c0a23139e6@edu.teu.ac.jp>
2024-09-24 17:32:31 +0900

    演習課題:

--- flying_kokaton.py
+++ flying_kokaton.py
@@ -16,20 +16,24 @@ def main():
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300,200 
     tmr = 0
+    dx = 0
+    dy = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-
+        dx = 0
+        dy = 0
         key_list = pg.key.get_pressed()
         if key_list[pg.K_UP]:
-            kk_rct.move_ip((0,-1)) 
+            dy = -1 
         if key_list[pg.K_DOWN]:
-            kk_rct.move_ip((0,+1))
+            dy = 1
         if key_list[pg.K_RIGHT]:
-            kk_rct.move_ip((+1,0))
+            dx = 2
         if key_list[pg.K_LEFT]:
-            kk_rct.move_ip((-1,0))
-        
+            dx = -1
+        kk_rct.move_ip((dx,dy))
+        kk_rct.move_ip((-1,0))
         x = -(tmr%3200)
         screen.blit(bg_img, [x, 0])
         screen.blit(bg_img2,[x+1600,0])
@@ -39,7 +43,7 @@ def main():
         screen.blit(kk_img,kk_rct)
         pg.display.update()
         tmr += 1        
-        clock.tick(400)
+        clock.tick(200)
 
 
 if __name__ == "__main__":
