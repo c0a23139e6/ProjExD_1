eb200f6a7f723dd83a182250bdb3529bd069a31d
proje <c0a23139e6@edu.teu.ac.jp>
2024-09-24 15:35:00 +0900

    練習5 :

--- pygame_image.py
+++ pygame_image.py
@@ -9,7 +9,7 @@ def main():
     pg.display.set_caption("はばたけ！こうかとん")
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
-    bg_img = pg.image.load("fig/pg_bg.jpg")
+    kk_img = pg.image.load("fig/pg_bg.jpg")
     kk_img = pg.image.load("fig/3.jpg")
     kk_img = pg.transform.flip(kk_img,True,False)
     tmr = 0
@@ -21,7 +21,7 @@ def main():
         screen.blit(bg_img,[0,0])
         pg.display.update()
         tmr += 1        
-        clock.tick(10)
+        clock.tick(200)
 
 
 if __name__ == "__main__":
