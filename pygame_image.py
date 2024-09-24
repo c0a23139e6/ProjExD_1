74dbe52653c4143007a5e88125b39e7138733727
proje <c0a23139e6@edu.teu.ac.jp>
2024-09-24 15:46:30 +0900

    練習1　と3 :

--- pygame_image.py
+++ pygame_image.py
@@ -9,16 +9,16 @@ def main():
     pg.display.set_caption("はばたけ！こうかとん")
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
-    kk_img = pg.image.load("fig/pg_bg.jpg")
-    kk_img = pg.image.load("fig/3.jpg")
+    bg_img = pg.image.load("fig/pg_bg.jpg")
+    kk_img = pg.image.load("fig/3.png")
     kk_img = pg.transform.flip(kk_img,True,False)
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-
-        screen.blit(kk_img, [300, 200])
-        screen.blit(bg_img,[0,0])
+        x = -(tmr%800)
+        screen.blit(bg_img, [x, 0])
+        screen.blit(kk_img,[300,200])
         pg.display.update()
         tmr += 1        
         clock.tick(200)
