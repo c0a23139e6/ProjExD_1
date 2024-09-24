3f6a492b2eb0f759f7b0503c82c46255019c8277
proje <c0a23139e6@edu.teu.ac.jp>
2024-09-24 16:49:14 +0900

    練習 8:

--- pygame_image.py
+++ pygame_image.py
@@ -29,14 +29,14 @@ def main():
             kk_rct.move_ip((+1,0))
         if key_list[pg.K_LEFT]:
             kk_rct.move_ip((-1,0))
-        x = -(tmr%1600)
+        
+        x = -(tmr%3200)
         screen.blit(bg_img, [x, 0])
         screen.blit(bg_img2,[x+1600,0])
         screen.blit(bg_img,[x+3200,0])
         screen.blit(bg_img2,[x+4800,0])
-        screen.blit(kk_img,[300,200])
+        #screen.blit(kk_img,[300,200])
         screen.blit(kk_img,kk_rct)
-        screen.blit()
         pg.display.update()
         tmr += 1        
         clock.tick(400)
